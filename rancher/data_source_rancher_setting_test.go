5d5a73efa7133942198c167e358ef8c479558bdb
Raphaël Pinson <raphael.pinson@camptocamp.com>
2017-07-03 14:16:49 +0200

    rancher_setting: add acceptance test

---
+++ rancher/data_source_rancher_setting_test.go
@@ -0,0 +1,29 @@
+package rancher
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccRancherSettingDataSource_accessLog(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckRancherSettingDataSourceConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.rancher_setting.access_log", "value", "/dev/null"),
+				),
+			},
+		},
+	})
+}
+
+// Testing owner parameter
+const testAccCheckRancherSettingDataSourceConfig = `
+data "rancher_setting" "access_log" {
+	name = "access.log"
+}
+`
