6295155be36632cf9726d792a3aa2eb8ad4ecd6a
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-01-15 13:33:31 +0100

    Inject cert in API to test data source

--- rancher/data_source_rancher_certificate_test.go
+++ rancher/data_source_rancher_certificate_test.go
@@ -12,16 +12,47 @@ func TestAccRancherCertificateDataSource_foo(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckRancherCertificateDataSourceConfig,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.rancher_certificate.foo", "cn", "foo"),
-				),
+				Config: testAccCheckRancherCertificateConfig,
 			},
 		},
 	})
 }
 
 // Testing owner parameter
+const testAccCheckRancherCertificateConfig = `
+resource "rancher_certificate" "foo_res" {
+	name = "foo"
+	environment_id = "1a5"
+	cert = <<EOT
+-----BEGIN CERTIFICATE-----
+MIIB4TCCAYugAwIBAgIUEJP/YW4QmK/dt85L+gaNsfaJUhQwDQYJKoZIhvcNAQEL
+BQAwRTELMAkGA1UEBhMCQVUxEzARBgNVBAgMClNvbWUtU3RhdGUxITAfBgNVBAoM
+GEludGVybmV0IFdpZGdpdHMgUHR5IEx0ZDAeFw0yMDAxMTUxMjMyMDVaFw0yMDAy
+MTQxMjMyMDVaMEUxCzAJBgNVBAYTAkFVMRMwEQYDVQQIDApTb21lLVN0YXRlMSEw
+HwYDVQQKDBhJbnRlcm5ldCBXaWRnaXRzIFB0eSBMdGQwXDANBgkqhkiG9w0BAQEF
+AANLADBIAkEA6YGnZfYxwQOEk4L2ZcbsghBDTt7MD2+STKshmSv0yUfI0lhmogT+
+NzsHjGbP2onZV5Pw8mMy4Snu7D+0zm0q/wIDAQABo1MwUTAdBgNVHQ4EFgQURhK1
+Sh9akzaFPxpvsrB27AXKcVgwHwYDVR0jBBgwFoAURhK1Sh9akzaFPxpvsrB27AXK
+cVgwDwYDVR0TAQH/BAUwAwEB/zANBgkqhkiG9w0BAQsFAANBAH6Qf8dD8SHP9rNI
+JTsRiUYfDIJfGzCni3on3EdscBanEbb3LAAmWCI0fJ/tMbzAPdGcTyuK5mSrVhBr
++2EQTnw=
+-----END CERTIFICATE-----
+EOT
+    key = <<EOT
+-----BEGIN PRIVATE KEY-----
+MIIBVQIBADANBgkqhkiG9w0BAQEFAASCAT8wggE7AgEAAkEA6YGnZfYxwQOEk4L2
+ZcbsghBDTt7MD2+STKshmSv0yUfI0lhmogT+NzsHjGbP2onZV5Pw8mMy4Snu7D+0
+zm0q/wIDAQABAkEAlKwPaDTzgr/5pm4o8a5RIZK3OD1U0bMpBBWlo7+/8HLDoJnk
+d5ssmuzg8xOLadXhZnjgkUPk55cxyfzzKCNI4QIhAPyzEdn8GjxlKSgzfZ1pZ4gl
+JHkLpBKPYB5KwJpnGlTxAiEA7I5o94ypM7yf5rKMTvF/TkGRrXKha0HH/ZHHqYqd
+vu8CIHdAypvkrTzzQIkIQ6+VnpZRcPTu2W8o2mNxQ5OaNIMBAiBlrvWJ64nT9nHZ
+jchoKsDpV6ASKaMfYsBfzClCRJZ4OwIhALqR1ZVh2ktONmMdBk4h/Hpjuim0Y0EZ
+0cwepge3Oz6M
+-----END PRIVATE KEY-----
+EOT
+}
+`
+
 const testAccCheckRancherCertificateDataSourceConfig = `
 data "rancher_certificate" "foo" {
 	name = "foo"
